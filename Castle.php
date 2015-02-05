6f95086aa87952b509661ea60f43999958268ebb
Hector <hector@getseoshop.com>
2015-02-05 18:04:37 +0100

    Develop commit 2

--- Castle.php
+++ Castle.php
@@ -46,4 +46,25 @@ class Castle
     {
         $this->wallHeight = $wallHeight;
     }
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
 }
