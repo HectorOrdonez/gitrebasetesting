dd008b8ab2aae9d388ab5ed701a6857118527905
Hector <hector@getseoshop.com>
2015-02-06 10:14:05 +0100

    Develop 3

--- AnotherFileChanged.php
+++ AnotherFileChanged.php
@@ -1,8 +1,8 @@
 <?php
-echo 'I am a file1';
-echo 'I am a file2';
-echo 'I am a file3';
-echo 'I am a file4';
+echo 'I am a file1develop';
+echo 'I am a file2develop';
+echo 'I am a file3develop';
+echo 'I am a file4develop';
 
 echo 'I am a file1';
 echo 'I am a file2';
