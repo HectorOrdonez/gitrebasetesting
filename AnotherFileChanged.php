edb8c5775ae324304c4a6e61768b1cc415daeb00
Hector <hector@getseoshop.com>
2015-02-06 10:13:27 +0100

    D 2

--- AnotherFileChanged.php
+++ AnotherFileChanged.php
@@ -4,6 +4,11 @@ echo 'I am a file2';
 echo 'I am a file3';
 echo 'I am a file4';
 
+echo 'I am a file1';
+echo 'I am a file2';
+echo 'I am a file3';
+echo 'I am a file4';
+
 // changes
 
 // more changes
